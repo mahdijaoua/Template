77543873233ba7aca64dea85905231715ea20167
mahdijaoua <mahdi.jaoua@esprit.tn>
2024-05-16 23:13:13 +0100

    Add project files.

---
+++ AM.Infrastructure/AMContext.cs
@@ -0,0 +1,52 @@
+﻿
+using AM.ApplicationCore.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AM.Infrastructure
+{
+    public class AMContext: DbContext
+    {
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies();
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\mssqllocaldb;
+              Initial Catalog=AirportManagementDB;Integrated Security=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+        //public AMContext(DbContextOptions<AMContext> options) : base(options)
+        //{
+
+        //}
+
+        public AMContext()
+        {
+            //this.ChangeTracker.LazyLoadingEnabled = false;
+        }
+
+        public AMContext(DbContextOptions<AMContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+
+        }
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+        
+        }
+
+
+
+    }
+}
